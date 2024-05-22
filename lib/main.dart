ab91bdf381536496a987b3e566b21b300e626a11
Gabriel Gomes <ggomes@samaecaxias.com.br>
2024-05-22 17:58:38 -0300

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                color: Colors.red,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.cyan,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.pinkAccent,
+                height: 50,
+                width: 50,
+              ),
+              Container(
+                color: Colors.purple,
+                height: 50,
+                width: 50,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
