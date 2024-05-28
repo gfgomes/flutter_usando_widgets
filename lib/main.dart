ea41000495658b670ab71b76071c2c42918723a3
Gabriel Gomes <gabriel.fgomes@gmail.com>
2024-05-28 12:13:09 -0300

    Continuação do projeto das tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:my_first_app/difficulty.dart';
+import 'screens/initial_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool opaque = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,185 +16,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: false,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Container(),
-          title: Text('Tarefas'),
-        ),
-        body: AnimatedOpacity(
-          opacity: opaque ? 1 : 0,
-          duration: Duration(milliseconds: 800),
-          child: ListView(
-            children: [
-              const Task(
-                  "Aprender Flutter",
-                  "https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large",
-                  1),
-              const Task(
-                  "Aprender a andar de bike",
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtUFOIQ2wY1mB7r5Io35FTdVduQcXMcfD82r2D07p9ww&s",
-                  2),
-              const Task(
-                  "Aprender a meditar",
-                  "https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg",
-                  3),
-              const Task(
-                  "Ler",
-                  "https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg",
-                  4),
-              Task('Jogar',
-                  'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 5),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(
-              opaque ? Icons.remove_red_eye : Icons.remove_red_eye_outlined),
-          onPressed: () {
-            setState(() {
-              opaque = !opaque;
-            });
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class Task extends StatefulWidget {
-  final String nome;
-  final String foto;
-  final int dificuldade;
-
-  const Task(this.nome, this.foto, this.dificuldade, {super.key});
-
-  @override
-  State<Task> createState() => _TaskState();
-}
-
-class _TaskState extends State<Task> {
-  int nivel = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(4),
-                color: Colors.blue,
-              ),
-              height: 140,
-            ),
-            Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Colors.white,
-                  ),
-                  height: 100,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(4),
-                            color: Colors.grey,
-                          ),
-                          height: 100,
-                          width: 80,
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(4),
-                            child: Image.network(
-                              widget.foto,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: 200,
-                              child: Text(
-                                widget.nome,
-                                style: TextStyle(fontSize: 24),
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: Difficulty(task: widget),
-                            ),
-                          ],
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(right: 15),
-                          height: 52,
-                          width: 52,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                nivel++;
-                              });
-                            },
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                Icon(Icons.arrow_drop_up),
-                                Text(
-                                  'UP',
-                                  style: TextStyle(fontSize: 12),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ]),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Container(
-                        width: 200,
-                        child: LinearProgressIndicator(
-                          color: Colors.green,
-                          value: () {
-                            if (widget.dificuldade > 0) {
-                              double newLevel = nivel / widget.dificuldade;
-                              return newLevel / 10;
-                            }
-
-                            return 1.0;
-                          }(),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "Nível: $nivel",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      home: const InitialScreen(),
     );
   }
 }
