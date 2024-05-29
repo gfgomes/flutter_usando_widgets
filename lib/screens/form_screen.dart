eaf6d41431baaf21e9c90650e7978a1f475ae6c0
Gabriel Gomes <gabriel.fgomes@gmail.com>
2024-05-29 12:01:58 -0300

    curso flutter 2 - aula 04

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,25 +1,126 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class FormScreen extends StatelessWidget {
+class FormScreen extends StatefulWidget {
   const FormScreen({super.key});
 
   @override
+  State<FormScreen> createState() => _FormScreenState();
+}
+
+class _FormScreenState extends State<FormScreen> {
+  TextEditingController _nomeTarefaController = TextEditingController();
+  TextEditingController _dificuldadeTarefaController = TextEditingController();
+  TextEditingController _imagemTarefaController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Nova tarefa'),
-      ),
-      body: Center(
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.black12,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(width: 3, color: Colors.black)),
-          height: 875,
-          width: 430,
-          child: Column(
-            children: [],
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Nova tarefa'),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 3, color: Colors.black)),
+              height: 875,
+              width: 430,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _nomeTarefaController,
+                      textAlign: TextAlign.center,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Insira o nome da tarefa.' : null,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Nome da terefa',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _dificuldadeTarefaController,
+                      validator: (value) => value!.isEmpty ||
+                              int.tryParse(value) == null ||
+                              int.parse(value) > 5 ||
+                              int.parse(value) < 1
+                          ? 'Insira um valor entre 1 e 5.'
+                          : null,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Dificuldade da terefa',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      controller: _imagemTarefaController,
+                      validator: (value) =>
+                          value!.isEmpty ? 'Insira a URL da imagem.' : null,
+                      keyboardType: TextInputType.url,
+                      onChanged: (text) => setState(() {}),
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Imagem da terefa',
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(width: 2, color: Colors.blue),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        _imagemTarefaController.text,
+                        errorBuilder: (BuildContext context, Object error,
+                            StackTrace? stackTrace) {
+                          return Image.asset('assets/images/no_image.jpg');
+                        },
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        print(_nomeTarefaController.text);
+                        print(int.parse(_dificuldadeTarefaController.text));
+                        print(_imagemTarefaController.text);
+                      }
+                    },
+                    child: const Text('Adicionar'),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
